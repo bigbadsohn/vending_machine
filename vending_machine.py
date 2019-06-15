276146d6bae6a3f91890c07f570104b5d26d0f71
bigbadsohn <dennissohn.work@gmail.com>
2019-06-15 01:20:58 -0700

    printed items, fixed question checking function, finished Item Choosing Process

--- vending_machine.py
+++ vending_machine.py
@@ -16,42 +16,62 @@ items = {"cookie": 1.00, "coke": 0.75, "candy": 0.50}
 # question: string of question that is asked
 def inputCheck(question, options):
 
-    options.lower()
-
     user_input = input(question)
-    user_input.lower()
 
     while (user_input not in options):
+        print("That is not a valid input.")
         user_input = input(question)
 
+    return user_input
 
-#make a vending machine class?
 
-### BOOKEND PROMPTS ###
+# example items list
+items = {"cookies": 0.50, "coke": 0.50, "gatorade": 1.00, "candy": 0.10}
+
+
+### GLOBAL PROMPTS ###
 # welcome prompt
 welcome_prompt = "Welcome to Dennis's Vending Machine \n"
 # thank you prompt
 thankyou_prompt = "Thank you for visiting Dennis's Vending Machine! Come again."
 
+
+### INTRO PROCESS ###
 # list option of items user can purchase and price (from json file)
+print(welcome_prompt)
+print("Our items include: ")
+for key in items.keys():
+    print(key)
 
 
 ### BUYING PROCESS ###
 # ask user to choose which item they want
+pickItemQuestion = "\nWhat item would you like? "
+itemOptions = items.keys()
 
+user_item_choice = inputCheck(pickItemQuestion, itemOptions)
+user_item_cost = items.get(user_item_choice)
+
+# Why is print statement default to 2 lines when ctrl + s?
+# How to display 2 decimal places for cost?
+print("Ok you have selected " + user_item_choice +
+      ". Please pay $" + str(user_item_cost))
 
 
 ### PAYMENT PROCESS ###
-
 # list payment options to user
 pay_options_prompt = "Please pay with following 'n' = $0.05, 'd' = $0.10, 'q' = $0.25, 'b' = $1.00 \n"
+
 # hashmap of payment options
 paymentOptions = {'n': 0.05, 'd': 0.10, 'q': 0.25, 'b': 1.00}
 
+
 # ask user to enter money
+# inserted_money = inputCheck(pay_options_prompt, paymentOptions.items())
 
 
 # check if user inserted enough money for item
+user_money_credit = 0
 
 
 ### POST PROCESSING ###
